9b4935b724ad23dc24610b9d1288b6ab7a28336c
Araf Al Jami <clown1331@gmail.com>
2023-02-18 03:44:27 +0000

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# restaurents-api
---
+++ src/RestaurantsApi.IdentityServer/ClientStore.cs
@@ -0,0 +1,23 @@
+﻿namespace RestaurantsApi.IdentityServer
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using IdentityServer4.Models;
+    using IdentityServer4.Stores;
+    using RestaurantsApi.Repositories.Contracts;
+
+    public class ClientStore : IClientStore
+    {
+        private readonly IRepository _mongoRepository;
+
+        public ClientStore(IRepository mongoRepository)
+        {
+            _mongoRepository = mongoRepository;
+        }
+
+        public Task<Client> FindClientByIdAsync(string clientId)
+        {
+            return _mongoRepository.GetItemAsync<Client>(u => u.ClientId.Equals(clientId));
+        }
+    }
+}
